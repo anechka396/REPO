40979664ed35a02e1c83cbca2a2fa2289305f3db
anechka396 <anechka396@mail.ru>
2014-04-20 18:30:30 +0200

    LAB9_asm

---
+++ LAB9_asm/LAB9_asm/Source.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int value = 0;
+
+	cout << "Enter the value: ";
+	cin >> value;
+
+	int sum = 0;
+
+	_asm
+	{
+		     mov     eax, value
+	         mov     ebx, 2
+		     xor     edx, edx
+		     div     ebx
+			 mov     ebx, eax // counter
+			 xor     ecx, ecx // sum
+	    m1:  cmp     ebx, 0
+			 je      m3
+			 xor     edx, edx
+			 mov     eax, value
+			 div     ebx			 
+			 cmp     edx, 0
+			 je      m2
+			 dec     ebx
+			 jmp     m1
+		m2:  add     ecx, ebx
+			 dec     ebx
+			 jmp     m1
+		m3 : mov     sum, ecx
+	}
+
+	if (value == sum)
+		cout << "yes" << endl;
+	else
+		cout << "no" << endl;
+	
+	return 0;
+}
