670ce8b8a2b183dc57bcdd11069f6c88fde2d00b
anechka396 <anechka396@mail.ru>
2014-04-20 18:41:29 +0200

    commit

--- LAB9_asm/LAB9_asm/Source.cpp
+++ LAB9_asm/LAB9_asm/Source.cpp
@@ -13,25 +13,28 @@ int main()
 
 	_asm
 	{
-		    mov     eax, value
-			mov     ebx, 2
-		    xor     edx, edx
-		    div     ebx
-			mov     ebx, eax // counter
-			xor     ecx, ecx // sum
-	    m1: cmp     ebx, 0
-			je      m3
-			xor     edx, edx
-			mov     eax, value
-			div     ebx			 
-			cmp     edx, 0
-			je      m2
-			dec     ebx
-			jmp     m1
-		m2: add     ecx, ebx
-			dec     ebx
-			jmp     m1
-		m3: mov     sum, ecx
+		mov     eax, value
+		mov     ebx, 2
+		xor     edx, edx
+		div     ebx
+		mov     ebx, eax // counter
+		xor     ecx, ecx // sum
+	m1:
+		cmp     ebx, 0
+		je      m3
+		xor     edx, edx
+		mov     eax, value
+		div     ebx			 
+		cmp     edx, 0
+		je      m2
+		dec     ebx
+		jmp     m1
+	m2:
+		add     ecx, ebx
+		dec     ebx
+		jmp     m1
+	m3:
+		mov     sum, ecx
 	}
 
 	if (value == sum)
